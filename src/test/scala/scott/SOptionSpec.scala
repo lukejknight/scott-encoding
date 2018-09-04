f6aff998ce714b2f1a1500808d267849768a8cd2
lukejknight <lukeknight93@gmail.com>
2018-09-04 18:33:33 +1000

    Add test helpers for SOption

--- src/test/scala/scott/SOptionSpec.scala
+++ src/test/scala/scott/SOptionSpec.scala
@@ -6,15 +6,18 @@ class SOptionSpec extends WordSpec with MustMatchers {
 
   import SOption._
 
+  def none[A]: SOption[A] = new SOption[A] {
+    override def runOption[B](none: => B, some: A => B): B = none
+  }
+
+  def some[A](a: A): SOption[A] = new SOption[A] {
+    override def runOption[B](none: => B, some: A => B): B = some(a)
+  }
+
   "SOption" should {
     "be castable to scala.Option" in {
-      toOption(new SOption[Int] {
-        override def runOption[B](none: B, some: Int => B): B = none
-      }) === None
-
-      toOption(new SOption[Int] {
-        override def runOption[B](none: B, some: Int => B): B = some(4)
-      }) === Some(4)
+      toOption(none) === None
+      toOption(some(4)) === Some(4)
     }
 
     "be castable from scala.Option" in {
