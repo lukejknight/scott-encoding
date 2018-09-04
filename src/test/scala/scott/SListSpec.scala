bf0975e1e1eb41f05915df5b613a2d3153909ee0
lukejknight <lukeknight93@gmail.com>
2018-09-04 18:40:08 +1000

    Add test helpers for SList

--- src/test/scala/scott/SListSpec.scala
+++ src/test/scala/scott/SListSpec.scala
@@ -6,19 +6,18 @@ class SListSpec extends WordSpec with MustMatchers {
 
   import SList._
 
+  def nil[A]: SList[A] = new SList[A] {
+    override def runList[B](nil: => B, cons: A => SList[A] => B): B = nil
+  }
+
+  def cons[A](a: A)(l: SList[A]): SList[A] = new SList[A] {
+    override def runList[B](nil: => B, cons: A => SList[A] => B): B = cons(a)(l)
+  }
+
   "SList" should {
     "be castable to scala.collection.immutable.List" in {
-      toList(new SList[Int] {
-        override def runList[B](nil: => B, cons: Int => SList[Int] => B): B = nil
-      }) === List.empty[Int]
-
-      toList(new SList[Int] {
-        override def runList[B](nil: => B, cons: Int => SList[Int] => B): B = cons(1)(new SList[Int] {
-          override def runList[B](nil: => B, cons: Int => SList[Int] => B): B = cons(2)(new SList[Int] {
-            override def runList[B](nil: => B, cons: Int => SList[Int] => B): B = nil
-          })
-        })
-      }) === List(1, 2)
+      toList(nil[Int]) === List.empty[Int]
+      toList(cons(1)(cons(2)(nil))) === List(1, 2)
     }
 
     "be castable from scala.collection.immutable.List" in {
