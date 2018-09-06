a29ef8762505ea0985b58b1ae208e73f795704c6
lukejknight <lukeknight93@gmail.com>
2018-09-07 09:09:38 +1000

    Add SEither tests

--- src/test/scala/scott/SEitherSpec.scala
+++ src/test/scala/scott/SEitherSpec.scala
@@ -1,18 +1,47 @@
 package scott
 
-import org.scalatest.{MustMatchers, WordSpec}
+import org.scalatest.{EitherValues, MustMatchers, WordSpec}
 
-class SEitherSpec extends WordSpec with MustMatchers {
+class SEitherSpec extends WordSpec with MustMatchers with EitherValues {
 
   import SEither._
+  import SList._
+
+  val expectedRight = "Expected right but received left"
+  val expectedLeft = "Expected left but received right"
 
   "SEither" should {
-    "be castable to scala.util.Either" in {
-
+    "support casting to scala.util.Either" in {
+      toEither[Int, Int](left(0)).fold(_ => succeed, _ => fail(expectedLeft))
+      toEither[Int, Int](right(1)).fold(_ => fail(expectedRight), _ => succeed)
     }
 
-    "be castable from scala.util.Either" in {
+    "support casting from scala.util.Either" in {
+      fromEither[Int, Int](Left(0)).runEither(_ => succeed, _ => fail(expectedLeft))
+      fromEither[Int, Int](Right(1)).runEither(_ => fail(expectedRight), _ => succeed)
+    }
 
+    "support isLeft" in {
+      isLeft(left(0)) mustEqual true
+      isLeft(right(1)) mustEqual false
+    }
+    "support isRight" in {
+      isRight(left(0)) mustEqual false
+      isRight(right(1)) mustEqual true
+    }
+
+    "support partition" in {
+        partition[Int, Int](
+          cons(left[Int, Int](3))(
+            cons(right[Int, Int](5))(
+              cons(left[Int, Int](4))(
+                cons(right[Int, Int](6))(
+                  nil[SEither[Int, Int]]))))).runPair(
+          p1 => p2 => {
+            toList(p1) mustEqual List(3, 4)
+            toList(p2) mustEqual List(5, 6)
+          }
+        )
     }
   }
 
