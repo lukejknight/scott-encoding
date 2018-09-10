a4aef69953330666d9bb8a289f43ec4d8d18abef
lukejknight <lukeknight93@gmail.com>
2018-09-10 21:11:55 +1000

    Add SPair tests

--- src/test/scala/scott/SPairSpec.scala
+++ src/test/scala/scott/SPairSpec.scala
@@ -2,17 +2,36 @@ package scott
 
 import org.scalatest._
 
-class SPairSpec extends WordSpec with Matchers {
+class SPairSpec extends WordSpec with MustMatchers {
 
   import SPair._
 
   "SPair" should {
-    "be castable from scala.Tuple2" in {
-
+    "support casting to scala.Tuple2" in {
+      toPair(sPair(1, 2)) mustEqual (1, 2)
     }
-
-    "be castable to scala.Tuple2" in {
-
+    "support casting from scala.Tuple2" in {
+      fromPair((1, 2)).runPair(a => b => a + b mustEqual 3)
+    }
+    "support fst" in {
+      fst(sPair(1, 2)) mustEqual 1
+    }
+    "support snd" in {
+      snd(sPair(1, 2)) mustEqual 2
+    }
+    "support swap" in {
+      swap(sPair(1, 2)).runPair(a => b => {
+        a mustEqual 2
+        b mustEqual 1
+      })
+    }
+    "support curry" in {
+      curry[Int, Int, Int](sPair =>
+        fst(sPair) + snd(sPair)
+      )(1)(2) mustEqual 3
+    }
+    "support uncurry" in {
+      uncurry[Int, Int, Int](a => b => a + b)(sPair(1, 2)) mustEqual 3
     }
   }
 }
